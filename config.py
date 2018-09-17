c39d6404b1d7d4dbca573a07e6e53df43d127fb4
louyx <yanxiang.lou@gmail.com>
2018-09-18 05:58:26 +0800

    .

---
+++ config.py
@@ -0,0 +1,123 @@
+from libqtile.config import Key, Screen, Group, Drag, Click
+from libqtile.command import lazy
+from libqtile import layout, bar, widget
+
+try:
+    from typing import List  # noqa: F401
+except ImportError:
+    pass
+
+mod = "mod4"
+
+keys = [
+    Key([mod], "j", lazy.layout.down()),
+    Key([mod], "k", lazy.layout.up()),
+    Key([mod], "h", lazy.layout.left()),
+    Key([mod], "l", lazy.layout.right()),
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left()),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right()),
+
+    # Key([mod, "shift"], "m", lazy.layout.grow_left()),
+    # Key([mod, "shift"], "r", lazy.layout.grow_right()),
+    # Key([mod, "shift"], "n", lazy.layout.normalize()),
+
+    Key([mod], "space", lazy.layout.toggle_split()),
+
+    Key([mod],          "c", lazy.spawn("google-chrome")),
+    Key([mod],          "Return", lazy.spawn("gnome-terminal")),
+    Key([mod, "shift"], "Return", lazy.spawn("gnome-terminal")),
+
+    # Toggle between different layouts as defined below
+    Key([mod], "Tab", lazy.next_layout()),
+
+    Key([mod], "r", lazy.spawncmd()),
+    # Key([mod], "w", lazy.window.kill()),
+    Key([mod, "shift"], "r", lazy.restart()),
+    Key([mod, "shift"], "q", lazy.shutdown()),
+
+    # volume keys
+    #Key([], "XF86AudioRaiseVolume", lazy.spawn("amixer -c 0 -q set Master 5%+")),
+    #Key([], "XF86AudioLowerVolume", lazy.spawn("amixer -c 0 -q set Master 5%-")),
+    Key([], "XF86AudioRaiseVolume", lazy.spawn("pactl set-sink-volume 0 +5%")),
+    Key([], "XF86AudioLowerVolume", lazy.spawn("pactl set-sink-volume 0 -5%")),
+    Key([], "XF86AudioMute",        lazy.spawn("pactl set-sink-mute 0 toggle")),
+    # brightness
+    Key([], "XF86MonBrightnessUp",   lazy.spawn("xbacklight -inc 5%")),
+    Key([], "XF86MonBrightnessDown", lazy.spawn("xbacklight -dec 5%")),
+]
+
+groups = [Group(i) for i in "12345678"]
+
+for i in groups:
+    keys.extend([
+        # mod1 + letter of group = switch to group
+        Key([mod], i.name, lazy.group[i.name].toscreen()),
+
+        # mod1 + shift + letter of group = switch to & move focused window to group
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name)),
+    ])
+
+layouts = [
+    layout.columns.Columns(num_columns=2, border_width=1),
+    layout.Max()
+]
+
+widget_defaults = dict(
+    font='sans',
+    fontsize=12,
+    padding=3,
+)
+extension_defaults = widget_defaults.copy()
+
+screens = [
+    Screen(
+        top=bar.Bar(
+            [
+                widget.GroupBox(),
+                widget.Prompt(),
+                widget.WindowName(),
+                widget.CurrentLayout(),
+                widget.Volume(),
+                widget.Systray(),
+                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
+            ],
+            24,
+        ),
+    ),
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front())
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: List
+main = None
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating(float_rules=[
+    {'wmclass': 'confirm'},
+    {'wmclass': 'dialog'},
+    {'wmclass': 'download'},
+    {'wmclass': 'error'},
+    {'wmclass': 'file_progress'},
+    {'wmclass': 'notification'},
+    {'wmclass': 'splash'},
+    {'wmclass': 'toolbar'},
+    {'wmclass': 'confirmreset'},  # gitk
+    {'wmclass': 'makebranch'},  # gitk
+    {'wmclass': 'maketag'},  # gitk
+    {'wname': 'branchdialog'},  # gitk
+    {'wname': 'pinentry'},  # GPG key password entry
+    {'wmclass': 'ssh-askpass'},  # ssh-askpass
+])
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+
+wmname = "LG3D"
